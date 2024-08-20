44749d5361dca7ace125049e8b87766c434ba06b
fangdehui <javaeecoding@gmail.com>
2024-08-20 17:49:37 +0900

    Change to ReactDOM.hydrateRoot for more clearly

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
 const data = window.__INITIAL_DATA__; // 从 SSR 注入的数据
 console.log("window.__INITIAL_DATA__", window.__INITIAL_DATA__)
 
-root.render(
+ReactDOM.hydrateRoot(
+  rootElement,
   <BrowserRouter>
     <App serverData={data}/>
   </BrowserRouter>
